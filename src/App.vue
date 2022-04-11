248012e39b692926563e340ea95d2f0e32872516
Dash <123qwe@gmail.com>
2022-04-11 14:58:56 -0500

    WouldYouRather on Vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Would you rather...</h1>
+    <would-you-rather v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged">
+    </would-you-rather>
+    <p>{{userSelectionMessage}}</p>
+    <!---reference to a template-->
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data(){
+    return{
+      wyrQuestion:'Would you rather be able to text/type very fast or read very quickly?',
+      wyrAnswer1:'Type',
+      wyrAnswer2:'Read',
+      userSelectionMessage:' '
+    }
+  },
+  methods:{
+    answerChanged(choice){
+      this.userSelectionMessage=`Thanks! You chose ${choice}`
+    }
   }
 }
 </script>
 
 <style>
+body{
+  background: wheat;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wyr">
+    <h2>Please enter your choice!</h2>
+    <h3>{{question}}</h3>
+    
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="ChoiceMade"><label>{{answer1}}</label>
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="ChoiceMade"><label>{{answer2}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    question: String, 
+    answer1: String, 
+    answer2: String,
+  },
+  data(){
+    return{
+      choice:''
+    }
+  },
+  methods:{
+    ChoiceMade(){
+      console.log('choice')
+      this.$emit('answer-changed', this.choice)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.wyr{
+  border: 2px darkblue solid;
+  background: lightskyblue;
+  margin:10px
+}
+</style>
